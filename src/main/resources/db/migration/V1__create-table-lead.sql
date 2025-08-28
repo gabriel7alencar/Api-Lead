c74654c5ad9ea62cb712c3631a1428cb626989ac
gabriel7alencar <gabrielleste70@gmail.com>
2025-08-28 11:50:31 -0300

    Teste Api

--- src/main/resources/db/migration/V1__create-table-lead.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE lead(
-    id BIGINT NO NULL auto_increment,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    telefone VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-)
